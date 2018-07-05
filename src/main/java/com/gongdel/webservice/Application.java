3b32eb0657a90b42dd7b6a67a3c526c27bc3af4a
gonghojin <yolip51@naver.com>
2018-07-05 11:36:41 +0900

    Revert "Revert "Add : "Uncompleted" 운영,개발 환경 분리-Nginx 활용 무중단 배포 구하기[7단계]""
    This reverts commit 0bf4fcc1c1fb5aa8c362ae6b312797667af487e2.

--- src/main/java/com/gongdel/webservice/Application.java
+++ src/main/java/com/gongdel/webservice/Application.java
@@ -2,13 +2,20 @@ package com.gongdel.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Aduiting annotation 활성화
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml"
+            + ",/app/config/springboot-webservice/real-application.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 }
