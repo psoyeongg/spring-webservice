0bf4fcc1c1fb5aa8c362ae6b312797667af487e2
gonghojin <yolip51@naver.com>
2018-07-05 11:32:46 +0900

    Revert "Add : "Uncompleted" 운영,개발 환경 분리-Nginx 활용 무중단 배포 구하기[7단계]"
    This reverts commit 15971aad15c5aca53b09b902b90f67c2f0456cdc.

--- src/main/java/com/gongdel/webservice/Application.java
+++ src/main/java/com/gongdel/webservice/Application.java
@@ -2,20 +2,13 @@ package com.gongdel.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // JPA Aduiting annotation 활성화
 @SpringBootApplication
 public class Application {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml"
-            + ",/app/config/springboot-webservice/real-application.yml";
-
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(Application.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
--- src/main/java/com/gongdel/webservice/web/WebController.java
+++ src/main/java/com/gongdel/webservice/web/WebController.java
@@ -13,7 +13,7 @@ public class WebController {
     private PostsService postsService;
 
     /**
-     *   handlebars-spring-boot-starter 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 path와 뒤의 파일 확장자는 자동으로 지정된다.
+     *   handlebars-spring-boot-starter 덕분에 컨트롤러에서 문자열을 반호나할 때 앞의 path와 뒤의 파일 확장자는 자동으로 지정된다.
      *   (prefix: src/main/resources/template, suffix: .hbs)
      */
     @GetMapping("/")
