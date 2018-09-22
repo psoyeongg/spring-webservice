6ffdd2d09c31626e5a65983ec95374017131fc97
gonghojin <yolip51@naver.com>
2018-09-22 20:59:05 +0900

    Fix : application.yml, WebController

--- src/main/java/com/gongdel/webservice/web/WebController.java
+++ src/main/java/com/gongdel/webservice/web/WebController.java
@@ -22,4 +22,9 @@ public class WebController {
 
         return "main";
     }
+
+    @GetMapping("/main")
+    public String home(Model model) {
+        return "index";
+    }
 }
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -4,6 +4,7 @@ insert into posts (title, author, content, code, status, created_date, modified_
 insert into posts (title, author, content, code, status, created_date, modified_date) values ('테스트2', 'test2@gmail.com', '테스트2의 본문', '마크다운2', 'Y', now(), now());
 insert into posts (title, author, content, code, status, created_date, modified_date) values ('테스트3', 'test3@gmail.com', '테스트3의 본문', '마크다운3', 'Y', now(), now());
 insert into posts (title, author, content, code, status, created_date, modified_date) values ('테스트4', 'test4@gmail.com', '테스트4의 본문', '마크다운4', 'Y', now(), now());
+insert into posts (title, author, content, code, status, created_date, modified_date) values ('테스트5', 'test5@gmail.com', '테스트5의 본문', '마크다운5', 'Y', now(), now());
 
 /*insert into posts (title, author, content, created_date, modified_date) values ('테스트1', 'test1@gmail.com', '테스트1의 본문', now(), now());
 insert into posts (title, author, content, created_date, modified_date) values ('테스트2', 'test2@gmail.com', '테스트2의 본문', now(), now());*/
